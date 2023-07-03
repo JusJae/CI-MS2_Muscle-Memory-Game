da99fec599118b3505db77a1f8ac78710c4c7b00
Jus Jae <42322548+JusJae@users.noreply.github.com>
2023-07-03 20:37:37 +0100

    feat(add): muscle-memory game constructor

--- game.js
+++ game.js
@@ -31,6 +31,20 @@ class AudioController {
     }
 }
 
+class MuscleMemory {
+    constructor(totalTime, cards) {
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById('time-remaining');
+        this.ticker = document.getElementById('flips');
+        this.matches = document.getElementById('matches');
+        this.audioController = new AudioController();
+    }
+    
+}
+
+
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
